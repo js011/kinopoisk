6e4770dab0cb2ad56a16dae5bba5e115e16b1345
oceantraffic <vadim.ivanyuk.126@gmail.com>
2020-09-24 14:42:59 +0300

    commit

---
+++ src/components/Header/Authentication/Authentication.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default class Authentication extends React.Component {
+  render() {
+    return <div></div>
+  }
+}
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,16 +1,22 @@
 import React from 'react'
+import Authentication from './Authentication/Authentication'
 
-const Header = () => {
-  return (
-    <div className="container pt-3 pb-3">
-      <div className="title col-2">
-        <p className="title__name" style={{ visibility: 'hidden' }}>
-          Kino<span className="title__name-prefix">poisk</span>
-        </p>
+class Header extends React.Component {
+  render() {
+    return (
+      <div className="container pt-3 pb-3">
+        <div className="title col-2">
+          <p className="title__name">
+            Kino<span className="title__name-prefix">poisk</span>
+          </p>
+        </div>
+        <div className="search col-8"></div>
+        <div className="col-2">
+          <Authentication />
+        </div>
       </div>
-      <div className="search col-10"></div>
-    </div>
-  )
+    )
+  }
 }
 
 export default Header
